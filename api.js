25b02de772cdfb62ce28b2fe63909d006c649662
sharan97000 <sharangoudusa1@gmail.com>
2025-11-20 20:02:28 +0000

    lab 5_sharan

--- api.js
+++ api.js
@@ -1,14 +1,14 @@
-const path = require('path')
 const Products = require('./products')
+const Orders = require('./orders')
+const path = require('path')
 const autoCatch = require('./lib/auto-catch')
-
 /**
  * Handle the root route
  * @param {object} req
  * @param {object} res
-*/
-function handleRoot(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+ */
+function handleRoot (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'))
 }
 
 /**
@@ -16,70 +16,130 @@ function handleRoot(req, res) {
  * @param {object} req
  * @param {object} res
  */
-async function listProducts(req, res) {
-  // Extract the limit and offset query parameters
+async function listProducts (req, res) {
   const { offset = 0, limit = 25, tag } = req.query
-  // Pass the limit and offset to the Products service
-  res.json(await Products.list({
+  const products = await Products.list({
     offset: Number(offset),
     limit: Number(limit),
     tag
-  }))
+  })
+  res.json(products)
 }
-
 
 /**
  * Get a single product
  * @param {object} req
  * @param {object} res
  */
-async function getProduct(req, res, next) {
+async function getProduct (req, res, next) {
   const { id } = req.params
 
   const product = await Products.get(id)
-  if (!product) {
-    return next()
-  }
+  if (!product) return next()
 
   return res.json(product)
 }
 
 /**
  * Create a product
- * @param {object} req 
- * @param {object} res 
+ * @param {object} req
+ * @param {object} res
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
  * Edit a product
  * @param {object} req
  * @param {object} res
- * @param {function} next
  */
-async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
+async function editProduct (req, res, next) {
+  const product = await Products.edit(req.params.id, req.body)
+  res.json(product)
 }
 
 /**
  * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
+ * @param {object} req
+ * @param {object} res
  */
-async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+async function deleteProduct (req, res, next) {
+  const result = await Products.destroy(req.params.id)
+  res.json(result)
 }
 
+/**
+ * Create an order
+ * @param {object} req
+ * @param {object} res
+ */
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(order)
+}
+
+/**
+ * List orders
+ * @param {object} req
+ * @param {object} res
+ */
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    productId,
+    status
+  })
+
+  res.json(orders)
+}
+
+/**
+ * Get a single order
+ * @param {object} req
+ * @param {object} res
+ */
+async function getOrder (req, res, next) {
+  const order = await Orders.get(req.params.id)
+  if (!order) return next()
+  res.json(order)
+}
+
+/**
+ * Edit an order
+ * @param {object} req
+ * @param {object} res
+ */
+async function editOrder (req, res, next) {
+  const order = await Orders.edit(req.params.id, req.body)
+  res.json(order)
+}
+
+/**
+ * Delete an order
+ * @param {object} req
+ * @param {object} res
+ */
+async function deleteOrder (req, res, next) {
+  const result = await Orders.destroy(req.params.id)
+  res.json(result)
+}
 module.exports = autoCatch({
   handleRoot,
+  // products
   listProducts,
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
-});
+  deleteProduct,
+  // orders
+  createOrder,
+  listOrders,
+  getOrder,
+  editOrder,
+  deleteOrder
+})
--- app.js
+++ app.js
@@ -2,23 +2,32 @@ const express = require('express')
 const api = require('./api')
 const middleware = require('./middleware')
 const bodyParser = require('body-parser')
-
-
 // Set the port
 const port = process.env.PORT || 3000
 // Boot the app
 const app = express()
 // Register the public directory
-app.use(express.static(__dirname + '/public'));
-// register the routes
+app.use(express.static(__dirname + '/public'))
+// JSON body parsing
 app.use(bodyParser.json())
+// CORS (from your middleware)
 app.use(middleware.cors)
+// Root
 app.get('/', api.handleRoot)
+
+// PRODUCT routes
 app.get('/products', api.listProducts)
 app.get('/products/:id', api.getProduct)
+app.post('/products', api.createProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
-app.post('/products', api.createProduct)
+
+// ORDER routes
+app.post('/orders', api.createOrder)
+app.get('/orders', api.listOrders)
+app.get('/orders/:id', api.getOrder)
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id', api.deleteOrder)
+
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
-
